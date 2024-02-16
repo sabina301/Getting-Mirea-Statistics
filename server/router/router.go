ff3434f7e9f769034916fb5ea5434b88fff06326
Сабина <sabinazholobova@gmail.com>
2024-02-17 00:02:22 +0300

    Update router

---
+++ server/router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"getting-statistics-mirea/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter(hand *handler.Handler) *gin.Engine {
+	router := gin.New()
+	router.Static("/client", "../client")
+	router.POST("/", hand.GetResult)
+	router.GET("/", func(c *gin.Context) {
+		c.File("../client/index.html")
+	})
+
+	return router
+}
+
+func Start(router *gin.Engine, addr string) error {
+	return router.Run(addr)
+}
